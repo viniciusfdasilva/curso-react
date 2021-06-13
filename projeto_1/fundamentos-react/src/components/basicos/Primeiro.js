756847a21b1f37ea5809edf3967377b636329abd
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-12 22:46:48 -0300

    Sessão 2 - Video 15

--- projeto_1/fundamentos-react/src/components/basicos/Primeiro.js
+++ projeto_1/fundamentos-react/src/components/basicos/Primeiro.js
@@ -1,3 +1,12 @@
+import React from "react"
+
 export default function Primeiro(){
-    return 'Primeiro componente! '
+    const msg = 'Seja bem vindo(a)!!'
+
+    return (
+        <div>
+            <h2>Primeiro componente! </h2>
+            <p>{ msg }</p>
+        </div>
+    )
 }

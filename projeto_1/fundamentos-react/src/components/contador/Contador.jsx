b411c01b04b0fba6ecf760ddeb07bdb808125bd5
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-18 09:50:37 -0300

    Sessão 2 - Video 37.1

--- projeto_1/fundamentos-react/src/components/contador/Contador.jsx
+++ projeto_1/fundamentos-react/src/components/contador/Contador.jsx
@@ -7,7 +7,7 @@ class Contator extends Component{
 
         this.state = {
             numero: this.props.numero || 0,
-            passo: this.props.inicial || 10,
+            passo: this.props.inicial || 5,
         }
     }
 
@@ -23,12 +23,25 @@ class Contator extends Component{
         })
     }
 
+    setPasso = (e) => {
+        this.setState({
+            passo: +e.target.value,
+        })
+    }
+
     render(){
         return(
             <div>
                 <h2>Contador</h2>
                 <h3>O valor atual é {this.state.numero}</h3>
                 <div>
+                    <input 
+                        id="id_incrementador" 
+                        className="inc"
+                        type="number"
+                        value={this.state.passo}
+                        onChange={this.setPasso}
+                    /><br/><br/>
                     <button onClick={_ => this.inc()} className="botaoinc">+</button>
                     <button onClick={_ => this.dec()} className="botaodec">-</button>
                 </div>

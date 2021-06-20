2d3d11fe9d8968306afe020f11c0e491791a77ff
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-20 18:59:47 -0300

    Sessao 3 - Video 45

---
+++ projeto_1/calculadora/src/components/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './calculator.css'
+
+
+export default class Calculator extends Component{
+    render(){
+        return(
+            <div className="calculator">
+                
+            </div>
+        )
+    }
+}
--- projeto_1/calculadora/src/index.js
+++ projeto_1/calculadora/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './components/main/Calculator'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Calculator</h1>
+    <Calculator/>
   </React.StrictMode>,
   document.getElementById('root')
 );

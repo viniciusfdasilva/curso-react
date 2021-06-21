74c94f1779431ca4b5178a538d86aa87be5e8f16
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-21 11:52:09 -0300

    Sessao 3 - Video 49

--- projeto_1/calculadora/src/components/main/Calculator.jsx
+++ projeto_1/calculadora/src/components/main/Calculator.jsx
@@ -19,23 +19,44 @@ export default class Calculator extends Component{
     }
 
     setOperation(operation){
-        console.log(operation)
+        if(this.state.current === 0){
+            this.setState({operation, current: 1, clearMemory: true})
+        }else{
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try{
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            }catch(e){
+                values[0] = this.state.values[0]
+            }
+            
+            values[1] = 0
+            this.setState({
+                displayValue: values[0], 
+                operation: equals ? null : operation, 
+                current: equals ? 0 : 1,
+                clearMemory: !equals,
+                values
+            })
+        }
     }
 
     adicionarDigito(n){
-        if(n == '.' && this.state.displayValue.includes('.')){
+        if(n === '.' && this.state.displayValue.includes('.')){
             return
         }
 
         const clearDisplay = this.state.displayValue === '0' || this.state.clearMemory
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
-        this.setState({displayValue, clearDisplay: false})
+        this.setState({displayValue, clearMemory: false})
 
         if(n != '.'){
             const i = this.state.current
             const newValue = parseFloat(displayValue)
-            const values = [this.state.values]
+            const values = [...this.state.values]
             values[i] = newValue
             this.setState({values})
         }

3a1d16fce9254f70a1d1dfcbe1d29b5ba585dd7e
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-21 10:57:03 -0300

    Sessao 3 - Video 48

--- projeto_1/calculadora/src/components/main/Calculator.jsx
+++ projeto_1/calculadora/src/components/main/Calculator.jsx
@@ -3,9 +3,19 @@ import './calculator.css'
 import Button from './../Button'
 import Display from '../../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearMemory: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 export default class Calculator extends Component{
+    state =  {...initialState}
+
     clearMemory(){
-        console.log('limpar')
+        this.setState({...initialState})
     }
 
     setOperation(operation){
@@ -13,7 +23,23 @@ export default class Calculator extends Component{
     }
 
     adicionarDigito(n){
-        console.log(n)
+        if(n == '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearMemory
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({displayValue, clearDisplay: false})
+
+        if(n != '.'){
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [this.state.values]
+            values[i] = newValue
+            this.setState({values})
+        }
+
     }
 
     render(){
@@ -22,7 +48,7 @@ export default class Calculator extends Component{
 
         return(
             <div className="calculator">
-                <Display value={100}/>
+                <Display value={this.state.displayValue}/>
                 <Button label="AC"click={() => this.clearMemory()} triple/>
                 <Button label="/" click={setOperation} operation/>
                 <Button label="7" click={adicionarDigito}/>

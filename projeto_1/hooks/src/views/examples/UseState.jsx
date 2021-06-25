e93f6c83499d290f9113cb8acbeb3f2aae645718
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-25 14:50:56 -0300

    Seção 5 - Video 63

--- projeto_1/hooks/src/views/examples/UseState.jsx
+++ projeto_1/hooks/src/views/examples/UseState.jsx
@@ -4,7 +4,7 @@ import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
     const [count, setCount] = useState(0)
-    
+    const [name, setName] = useState('')
 
     return (
         <div className="UseState">
@@ -21,6 +21,8 @@ const UseState = (props) => {
                 </div>
             </div>
             <SectionTitle title='Exercício #02'/>
+            <input type="text" className="input" value={name} onChange={e => setName(e.target.value)}/>
+            <span className="text">{name}</span>
         </div>
     )
 }

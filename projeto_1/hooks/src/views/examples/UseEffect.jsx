44b7bc14c27a455a4fd9a5ec733efa13155747ce
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-26 23:06:15 -0300

    Seção 5 - Video 65

--- projeto_1/hooks/src/views/examples/UseEffect.jsx
+++ projeto_1/hooks/src/views/examples/UseEffect.jsx
@@ -6,13 +6,23 @@ function calcFatorial(valor){
     if(valor === 0) return 1
     return calcFatorial(valor-1) * valor;
 }
+
+function isPar(valor){
+    return (valor % 2 === 0) ? true : false
+}
 const UseEffect = (props) => {
     const [valor, setValor] = useState(1)
     const [fatorial, setFatorial] = useState(1)
+    const [parouimpar,setParouImpar] = useState(false)
+
+    useEffect(() => {
+        setParouImpar(isPar(valor))
+    },[valor])
 
     useEffect(() => {
         setFatorial(calcFatorial(valor))
     }, [valor])
+
     return (
         <div className="UseEffect">
             <PageTitle
@@ -24,14 +34,18 @@ const UseEffect = (props) => {
                     <span className="text">Fatorial: </span>
                     <span className="text red">{fatorial == -1 ? "Dado inválido" : fatorial}</span>
                 </div>
+
+                <div>
+                    <span className="text">Par ou impar:</span>
+                    <span className="text red">{parouimpar ? "PAR" : "ÍMPAR"}</span>
+                </div>
                 
                 <input 
                     type="number"   
                     className="input" 
                     value={valor} 
-                    onChange={e => 
-                    setValor(e.target.value)}
-                />
+                    onChange={e => setValor(e.target.value)}
+                />               
             </div>
         </div>
     )

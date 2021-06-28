2d8dafc5dd9933aae153409d42e82c204e4f2e81
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-28 11:46:09 -0300

    Seção 5 - Video 69

--- projeto_1/hooks/src/views/examples/UseRef.jsx
+++ projeto_1/hooks/src/views/examples/UseRef.jsx
@@ -2,19 +2,29 @@ import React, {useState, useRef, useEffect} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+
+function merge(str1,str2){
+    return [...str1].map((e,i) => {
+        return `${e}${str2[i] || ''}`
+    }).join('')
+}
+
 const UseRef = (props) => {
     var [value1, setValue1] = useState('')
-    const [value2,setValue2] = useState("")
+    const [value2,setValue2] = useState('')
     const count = useRef(0)
+    
 
     useEffect(() => {
         count.current = count.current+1
         myInput2.current.focus()
+       
     },[value1])
 
     useEffect(() => {
         count.current = count.current+1
         myInput1.current.focus()
+       
     },[value2])
 
     const myInput1 = useRef(null)
@@ -31,7 +41,7 @@ const UseRef = (props) => {
             <div className="center">
                 <div>
                     <span className="text">Valor: </span>
-                    <span className="text"> {value1} [</span>
+                    <span className="text"> {merge(value1,value2)} [</span>
                     <span className="text red">{count.current}</span>
                     <span className="text">]</span>
                 </div>

63bb78c70300d41b8a7668d98a16c372ceaa28e2
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-29 11:49:43 -0300

    Seção 5 - Video 73

--- projeto_1/hooks/src/views/examples/UseContext.jsx
+++ projeto_1/hooks/src/views/examples/UseContext.jsx
@@ -6,6 +6,7 @@ import DataContext from '../../data/DataContext'
 const UseContext = (props) => {
 
     const {state, setState} = useContext(DataContext)
+    const [value, setValue] = useState(0)
 
     function setNumber(delta, op){
         setState({
@@ -26,9 +27,15 @@ const UseContext = (props) => {
                 <span className="text">{state.text}</span>
                 <span className="text">{state.number}</span>
 
+                <input type="number" 
+                       className="input" 
+                       value={value} 
+                       onChange={e => setValue(e.target.value === "" ? 0 : e.target.value)}
+                />
+                
                 <div>
-                    <button className="btn" onClick={() => setNumber(1,"+")}>+1</button>
-                    <button className="btn" onClick={() => setNumber(1,"-")}>-1</button>
+                    <button className="btn" onClick={() => setNumber(parseInt(value),"+")}>+1</button>
+                    <button className="btn" onClick={() => setNumber(parseInt(value),"-")}>-1</button>
                 </div>
             </div>
         </div>

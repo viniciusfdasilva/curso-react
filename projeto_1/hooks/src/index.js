28ca964032d1b00373eb5eb4cb06d6756d3cf4b7
vinicius <vinicius.silva.1046664@sga.pucminas.br>
2021-06-29 11:40:00 -0300

    Seção 5 - Video 72

--- projeto_1/hooks/src/index.js
+++ projeto_1/hooks/src/index.js
@@ -4,7 +4,9 @@ import React from 'react'
 
 import App from './views/App'
 
+ 
+ 
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById('root')
 )
--- projeto_1/hooks/src/views/examples/UseContext.jsx
+++ projeto_1/hooks/src/views/examples/UseContext.jsx
@@ -1,7 +1,20 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../data/DataContext'
+
 const UseContext = (props) => {
+
+    const {state, setState} = useContext(DataContext)
+
+    function setNumber(delta, op){
+        setState({
+            ...state,
+            number: op === "+" ? state.number + delta : 
+                    op === "-" ? state.number - delta : state.number 
+        })
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
@@ -9,7 +22,15 @@ const UseContext = (props) => {
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
 
-            
+            <div className="center">
+                <span className="text">{state.text}</span>
+                <span className="text">{state.number}</span>
+
+                <div>
+                    <button className="btn" onClick={() => setNumber(1,"+")}>+1</button>
+                    <button className="btn" onClick={() => setNumber(1,"-")}>-1</button>
+                </div>
+            </div>
         </div>
     )
 }
